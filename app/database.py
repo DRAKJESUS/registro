ce1ca99273e12ad956c3653649e34862bd2871eb
jesus <angelhernan12486@gmail.com>
2025-07-10 14:05:47 -0600

    Pots

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import DATABASE_URL
+from typing import AsyncGenerator
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
+
+# ✅ ESTA FUNCIÓN ES LA QUE FALTABA
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
