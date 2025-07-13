e480e4693cfe048517e3640b14457701e0551bbd
jesus <angelhernan12486@gmail.com>
2025-07-12 20:02:36 -0600

    cambio_8

--- app/models/assignment_model.py
+++ app/models/assignment_model.py
@@ -6,7 +6,7 @@ class AssignmentHistory(Base):
     __tablename__ = "assignment_history"
 
     id = Column(Integer, primary_key=True, index=True)
-    device_id = Column(Integer, ForeignKey("devices.id"), nullable=False)
+    device_id = Column(Integer, ForeignKey("devices.id", ondelete="CASCADE"), nullable=False)
     action = Column(String, nullable=False)
     old_location_id = Column(Integer, nullable=True)
     new_location_id = Column(Integer, nullable=True)
--- app/models/port_model.py
+++ app/models/port_model.py
@@ -6,8 +6,8 @@ class Port(Base):
     __tablename__ = "ports"
 
     id = Column(Integer, primary_key=True, index=True)
-    port_number = Column(Integer)
-    description = Column(String)
+    port_number = Column(Integer, nullable=False)
+    description = Column(String, nullable=True)
 
-    device_id = Column(Integer, ForeignKey("devices.id"))
-    device = relationship("Device", back_populates="ports")
+    device_id = Column(Integer, ForeignKey("devices.id", ondelete="CASCADE"), nullable=False)
+    device = relationship("Device", back_populates="ports", lazy="selectin")
